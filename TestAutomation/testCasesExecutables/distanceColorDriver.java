6462b8b208edaad235497110bed9346f8e882a89
binghamjw <31294851+binghamjw@users.noreply.github.com>
2017-11-13 00:03:58 -0500

    Update distanceColorDriver
    driver is working and stable with the exception of improperly comparing doubles and always returning false, even when the two values are equal upon manual observation.

--- TestAutomation/testCasesExecutables/distanceColorDriver.java
+++ TestAutomation/testCasesExecutables/distanceColorDriver.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package contrastfinder;
-import static contrastfinder.DistanceCalculator.calculate;
+
 import java.awt.Color;
+import java.io.*;
+import java.lang.NullPointerException;
+import org.opens.utils.distancecalculator.DistanceCalculator;
 /**
  *
  * @author omer
@@ -15,39 +17,70 @@ public class distanceColorDriver {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-    	if(args.length == 3)
+    public static void main(String[] args) throws IOException {
+    	int methodToTest=-1;
+        String color1Values="";
+        String color2Values="";
+        double oracle = 0;
+        String errOracle = "";
+        if(args.length == 4)
     	{
-    		String color1Values = args[0];
-    		String color2Values = args[1];
-    		double oracle = args[2];
+    		color1Values = args[1];
+    		color2Values = args[2];
+    		oracle = Double.parseDouble(args[3]);
+    	}
+    	else if(args.length == 3)
+    	{
+    		color1Values = args[1];
+    		errOracle = args[2];
     	}
     	else if(args.length == 2)
     	{
-    		String color1Values = args[0];
-    		double oracle = args[1];
+    		errOracle = args[1];
     	}
-    	else if(args.length == 1)
-    	{
-    		double oracle = args[0];
-    	}
-    	String[] color1Val = split(color1Values,',');
-    	String[] color2Vals = split(color2Values,',');
-        Color color1 = new Color(color1Val[0], color1Val[1], color1Val[2]);
-        Color color2 = new Color(color2Val[0], color2Val[1], color2Val[2]);
-        double result = calculate(color1, color2));
-		if(result == oracle)
+        Color color1;
+        Color color2;
+        if(color1Values != ""){
+           	String[] color1Val = color1Values.split(",");
+            color1 = new Color(Integer.parseInt(color1Val[0]), Integer.parseInt(color1Val[1]), Integer.parseInt(color1Val[2]));
+        }
+        else{
+            color1 = null;
+        }
+        if(color2Values != ""){
+            String[] color2Val = color2Values.split(",");
+            color2 = new Color(Integer.parseInt(color2Val[0]), Integer.parseInt(color2Val[1]), Integer.parseInt(color2Val[2]));
+        }
+        else{
+            color2 = null;
+        }
+        String passfail ="";
+        double result = -1;
+        System.out.println(errOracle);
+        try{
+            result = DistanceCalculator.calculate(color1, color2);
+            System.out.println(result);
+        }
+        catch(NullPointerException error){
+            if(errOracle.compareToIgnoreCase("Error") == 0){
+                passfail = "true";
+            }
+            else{
+                passfail = "false";
+            }
+        }
+        System.out.println(passfail);
+		if((Double.compare(result, oracle) == 0) && passfail == "")
 		{
-			String passfail = "true"
+			passfail = "true";
 		}
-		else
+		else if(Double.compare(result, oracle) != 0 && passfail == "")
 		{
-			String passfail = "false"
+			passfail = "false";
 		}
 		PrintWriter writer = new PrintWriter("./../temp/result.txt", "UTF-8");
 		writer.println(passfail);
-		writer.close();
-         
+		writer.close();         
     }
     
 }
